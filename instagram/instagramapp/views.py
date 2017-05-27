b57e850aeaea2ff0a239b3631e68a310e02d5c58
Juan Esteban <jevelasro@gmail.com>
2017-05-27 12:57:00 -0500

    cuarta entrada 27mayo octavasecion

--- instagram/instagramapp/views.py
+++ instagram/instagramapp/views.py
@@ -1,18 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from instagramapp.models import *
 
 # Create your views here.
 def registro(request):
-    return render( request, 'registro.html')
+    return render( request, 'registro_del_instagram.html')
 
 def crear_usuario ( request ):
-    email=request
-    username = request.POST['Nombre de Usuario']
-     name=(request.POST['Nombre Completo']
-    return render( request, 'pag_inicio_instagram.html')
-    print(email)
-    print(username)
-    print(password)
-    print(name)
+    _email=request.POST['email']
+    _username = request.POST['username']
+    _name=request.POST['name']
+    _password=request.POST['password']
+    print (_email)
+    print (_username)
+    print (_password)
+    print (_name)
+    user=User .objects.create_user(username= _username, email=_email,
+                                    password=_password, first_name=_name )
+    myUser= MiUsuario( usuario=user )
+    myUser.save
+    print (user)
+    print (user.password)
+    return redirect('tienes_cuenta')
+
 
 def tienes_cuenta(request):
     return render( request, 'tienes_cuenta.html')
