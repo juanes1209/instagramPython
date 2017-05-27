135401249676ad741430fe05a0e4861482d2f592
Centro Ático <centroatico@202C11E26168967.local>
2017-05-27 09:23:38 -0500

    tercera entrada 20demayo

--- instagram/instagramapp/urls.py
+++ instagram/instagramapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
    url (r'^$', views.registro, name = 'registro'),
+   url (r'^crear_usuario/$',views.crear_usuario, name='crear_usuario' )
    url (r'^tienes_cuenta/$',views.tienes_cuenta, name='tienes_cuenta' )
 ]
--- instagram/instagramapp/views.py
+++ instagram/instagramapp/views.py
@@ -4,5 +4,15 @@ from django.shortcuts import render
 def registro(request):
     return render( request, 'registro.html')
 
+def crear_usuario ( request ):
+    email=request
+    username = request.POST['Nombre de Usuario']
+     name=(request.POST['Nombre Completo']
+    return render( request, 'pag_inicio_instagram.html')
+    print(email)
+    print(username)
+    print(password)
+    print(name)
+
 def tienes_cuenta(request):
     return render( request, 'tienes_cuenta.html')
